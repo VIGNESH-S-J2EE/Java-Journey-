03454d11790b17383aeabffaa59a8763254b6d1a
VIGNESH-S-J2EE <vigneshjava69@gmail.com>
2025-05-03 18:30:09 +0530

    Add More Mini Projects

--- src/JavaBasics/LoopsAndMethods/PowerSeries/Power_Series.java
+++ src/JavaBasics/LoopsAndMethods/PowerSeries/Power_Series.java
@@ -44,4 +44,6 @@ public class Power_Series {
 git add README.md
 git commit -m "first commit"
 git branch -M main
+* git remote add origin https://github.com/VIGNESH-S-J2EE/Animation-Loading.git
+git push -u origin main
 * */
